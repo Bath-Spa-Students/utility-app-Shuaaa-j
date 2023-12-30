471efa8dfc91f81a7c7acf341f2114a2d6d92d7a
Shuaaa-j <JoshuaGofulco246@gmail.com>
2023-12-30 19:33:41 +0400

    Vending Machine.py

--- Vending Machine.py
+++ Vending Machine.py
@@ -3,14 +3,17 @@ from prettytable import PrettyTable
 
 class VendingMachine:
 
+    COUPON_CODE = "RAFIA"
+    DISCOUNT_PERCENTAGE = 10  # Adjust the discount percentage as needed
+
     def __init__(self):
         # Define menu items with codes, names, prices, categories, and stock
         self.menu = {
-            'A1': {'name': ' Coke', 'price': 1.50, 'category': 'Drinks', 'stock': 5},
-            'A2': {'name': ' Water', 'price': 1.00, 'category': 'Drinks', 'stock': 8},
-            'B1': {'name': ' Cheetos', 'price': 1.75, 'category': 'Chips', 'stock': 3},
-            'B2': {'name': ' Doritos', 'price': 2.00, 'category': 'Chips', 'stock': 4},
-            'C1': {'name': ' Kinder', 'price': 1.50, 'category': 'Sweets', 'stock': 5},
+            'A1': {'name': 'Mountain Dew', 'price': 1.50, 'category': 'Drinks', 'stock': 5},
+            'A2': {'name': 'Water', 'price': 1.00, 'category': 'Drinks', 'stock': 8},
+            'B1': {'name': 'Cheetos', 'price': 1.75, 'category': 'Chips', 'stock': 3},
+            'B2': {'name': 'Doritos', 'price': 2.00, 'category': 'Chips', 'stock': 4},
+            'C1': {'name': 'Hershey', 'price': 1.50, 'category': 'Sweets', 'stock': 1},
             'C2': {'name': 'Snicker', 'price': 1.00, 'category': 'Sweets', 'stock': 8},
         }
 
@@ -19,78 +22,85 @@ class VendingMachine:
 
     def display_menu(self):
         print(""" 
-██╗░░░██╗███████╗███╗░░██╗██████╗░██╗███╗░░██╗░██████╗░  ███╗░░░███╗░█████╗░░█████╗░██╗░░██╗██╗███╗░░██╗███████╗
-██║░░░██║██╔════╝████╗░██║██╔══██╗██║████╗░██║██╔════╝░  ████╗░████║██╔══██╗██╔══██╗██║░░██║██║████╗░██║██╔════╝
-╚██╗░██╔╝█████╗░░██╔██╗██║██║░░██║██║██╔██╗██║██║░░██╗░  ██╔████╔██║███████║██║░░╚═╝███████║██║██╔██╗██║█████╗░░    
-░╚████╔╝░██╔══╝░░██║╚████║██║░░██║██║██║╚████║██║░░╚██╗  ██║╚██╔╝██║██╔══██║██║░░██╗██╔══██║██║██║╚████║██╔══╝░░
-░░╚██╔╝░░███████╗██║░╚███║██████╔╝██║██║░╚███║╚██████╔╝  ██║░╚═╝░██║██║░░██║╚█████╔╝██║░░██║██║██║░╚███║███████╗
-░░░╚═╝░░░╚══════╝╚═╝░░╚══╝╚═════╝░╚═╝╚═╝░░╚══╝░╚═════╝░  ╚═╝░░░░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝╚══════╝ 
-         ________________________  
-        |  ____________________  |
-        | | []  []  []  []  [] | |
-        | |--------------------| |
-        | | []  []  []  []  [] | |
-        | |--------------------| |
-        | |____________________| |
-        | |  ________________  | |
-        | | | COLLECT        | | |
-        | | |           HERE | | |
-        | |  ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅   | |
-        |  ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅   |
-        |̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ |
-        |_|̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ |_|""")
+█░█ █▀▀ █▄░█ █▀▄ █ █▄░█ █▀▀   █▀▄▀█ ▄▀█ █▀▀ █░█ █ █▄░█ █▀▀
+▀▄▀ ██▄ █░▀█ █▄▀ █ █░▀█ █▄█   █░▀░█ █▀█ █▄▄ █▀█ █ █░▀█ ██▄
+             ________________________  
+            |  ____________________  |
+            | | []      []      [] | |
+            | |--------------------| |
+            | | []      []      [] | |
+            | |--------------------| |
+            | |____________________| |
+            | |  ________________  | |
+            | | | COLLECT        | | |
+            | | |           HERE | | |
+            | |  ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅   | |
+            |  ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅   |
+            |̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ |
+            |_|̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ ̅ |_|""")
         table = PrettyTable()
         table.field_names = ["Code", "Item", "Price", "Category", "Stock"]
         for code, item in self.menu.items():
-            table.add_row([code, item['name'], f"${item['price']:.2f}", item['category'], item['stock']])
+            table.add_row([code, item['name'], f"AED {item['price']:.2f}", item['category'], item['stock']])
         print(table)
 
     def accept_money(self):
         while True:
             try:
-                money = float(input("Insert money: AED"))
+                money = float(input("Insert money: AED "))
                 if money >= 0:  # Allow inserting zero for the remaining balance
+                    coupon_code = input("\nEnter coupon code (Input NA if no coupon code is available): ").upper()
+                    if coupon_code == self.COUPON_CODE:
+                        print(f"Applied {self.DISCOUNT_PERCENTAGE}% discount!")
+                        money *= (1 - self.DISCOUNT_PERCENTAGE / 100)
                     return money
                 else:
                     print("Invalid amount. Please insert a valid amount.")
             except ValueError:
                 print("Invalid input. Please enter a number.")
 
+    def add_more_money(self):
+        additional_money = self.accept_money()
+        self.balance += additional_money
+
     def select_item(self):
-        code = input("Enter the code: ").upper()
-        if code in self.menu:
-            item = self.menu[code]
-            if item['stock'] > 0 and item['price'] <= self.balance:
-                self.balance -= item['price']
-                item['stock'] -= 1
-                print(f"\nDispensing {item['name']}...")
-                time.sleep(2)
-                print("\nEnjoy your", item['name'] + "!")
-                self.suggest_purchase(item['category'])
-                self.display_change()
-                self.buy_additional_item()
-                self.purchased_items.append({'item_name': item['name'], 'item_price': item['price']})
-            elif item['stock'] == 0:
-                print("Sorry, this item is out of stock.")
+        while True:
+            code = input("\nEnter the code (or 'exit' to stop): ").upper()
+            if code == 'EXIT':
+                break
+            if code in self.menu:
+                item = self.menu[code]
+                while item['stock'] > 0 and item['price'] > self.balance:
+                    print(f"Insufficient funds for {item['name']}.")
+                    self.add_more_money()
+                if item['stock'] > 0:
+                    self.balance -= item['price']
+                    item['stock'] -= 1
+                    print(f"\nDispensing {item['name']}...")
+                    time.sleep(2)
+                    print("\nEnjoy your", item['name'] + "!")
+                    self.suggest_purchase(item['category'])
+                    self.display_change()
+                    self.purchased_items.append({'item_name': item['name'], 'item_price': item['price']})
+                else:
+                    print("Sorry, this item is out of stock.")
             else:
-                print("Insufficient funds. Please insert more money.")
-        else:
-            print("Invalid code. Please enter a valid code.")
+                print("Invalid code. Please enter a valid code.")
 
     def display_change(self):
         if self.balance > 0:
-            print(f"Remaining Balance: ${self.balance:.2f}")
+            print(f"Remaining Balance: AED {self.balance:.2f}")
 
-    def return_change(self):    
+    def return_change(self):
         if self.balance > 0:
-            print(f"Returning change: ${self.balance:.2f}")
+            print(f"Returning change: AED {self.balance:.2f}")
             time.sleep(1)
 
     def suggest_purchase(self, category):
         suggestions = {
             'Drinks': 'How about adding some snacks like Chips?',
-            'Snacks': 'Would you like to try a refreshing drink like Water?',
-            'Sweets': 'Would you like Water?'
+            'Snacks': 'Some sweets would go well after eating chips!',
+            'Sweets': 'We recommend drinks alongside your sweets as refreshment!'
         }
         if category in suggestions:
             print(suggestions[category])
@@ -104,7 +114,7 @@ class VendingMachine:
 
     def run_vending_machine(self):
         self.display_menu()
-        print(f"Current Balance: ${self.balance:.2f}")
+        print(f"Current Balance: AED {self.balance:.2f}")
         money_inserted = self.accept_money()
         self.balance += money_inserted
         self.select_item()
@@ -112,8 +122,8 @@ class VendingMachine:
     def create_receipt(self):
         receipt = "\n===== Receipt =====\n"
         for item in self.purchased_items:
-            receipt += f"\t{item['item_name']} -- ${item['item_price']:.2f}\n"
-        receipt += f"\tTotal --- ${sum(item['item_price'] for item in self.purchased_items):.2f}\n"
+            receipt += f"{item['item_name']} -- AED {item['item_price']:.2f}\n"
+        receipt += f"Total --- AED {sum(item['item_price'] for item in self.purchased_items):.2f}\n"
         receipt += "===================\n"
         return receipt
 
@@ -121,10 +131,10 @@ if __name__ == "__main__":
     vending_machine = VendingMachine()
     vending_machine.run_vending_machine()
 
-    rec_bool = int(input("1. Print the receipt? 2. Only print the total sum: "))
-    if rec_bool == 1:
+    mica = input("Would you like a receipt? (yes/no): ").lower()
+    if mica == "no":
+        print(f"Total Sum: AED {sum(item['item_price'] for item in vending_machine.purchased_items):.2f}. Thank you for buying!")
+    elif mica == "yes":
         print(vending_machine.create_receipt())
-    elif rec_bool == 2:
-        print(f"Total Sum: ${sum(item['item_price'] for item in vending_machine.purchased_items):.2f}")
     else:
         print("INVALID ENTRY")
